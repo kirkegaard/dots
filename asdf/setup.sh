d282062437b185831305330e83ab7ff66a999c6b
Christian Kirkegaard <christian@lowpoly.dk>
2025-04-21 23:39:15 +0200

    Add asdf

---
+++ asdf/setup.sh
@@ -0,0 +1,9 @@
+asdf plugin-add deno https://github.com/asdf-community/asdf-deno.git
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin add bun
+asdf plugin add yarn
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+# asdf plugin-add php https://github.com/asdf-community/asdf-php.git
+# asdf plugin-add zig https://github.com/asdf-community/asdf-zig.git
+
+copy tool-versions ~/.tool-versions
